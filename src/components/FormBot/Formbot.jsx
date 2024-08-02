c5db4fd25a093d7780c70ffbcfdfa4e1848d9e50
Bharadwaj <haribalbharadwaj@gmail.com>
2024-08-02 00:42:36 -0700

    U5

--- src/components/FormBot/Formbot.jsx
+++ src/components/FormBot/Formbot.jsx
@@ -124,7 +124,6 @@ const Formbot = () => {
                                     type={type === 'number' ? 'number' : 'text'}
                                     value={input.value || ''}
                                     onChange={(e) => handleInputChange(type, index, e)}
-                                    placeholder={`Enter ${type}`}
                                     style={{ display: 'block', margin: '10px 0' }}
                                 />
                             </>
